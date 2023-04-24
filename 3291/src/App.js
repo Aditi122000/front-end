62df5b30611dd07541c473dee64d49e6638eccee
sumit anand <79632416+lostboyanand@users.noreply.github.com>
2023-04-24 23:40:23 +0530

    button

---
+++ 3291/src/App.js
@@ -0,0 +1,228 @@
+import React from 'react';
+import './App.css';
+import { Button } from "./Button";
+
+function App() {
+  return (
+    <div className="homepage-rotaract-homepage-rotaract-wrapper">
+    <div className="homepage-rotaract-homepage-rotaract">
+      <div className="homepage-rotaract-overlap">
+        <img className="homepage-rotaract-group" alt={"Group"} src={"group.png"} />
+        <img className="homepage-rotaract-img" alt={"Group"} src={"group-2.png"} />
+        <div className="homepage-rotaract-rectangle" />
+        <div className="homepage-rotaract-group-1">
+          <div className="homepage-rotaract-overlap-group3">
+            <div className="homepage-rotaract-overlap-group2">
+              <div className="homepage-rotaract-overlap-group1">
+                <div className="homepage-rotaract-ellipse" />
+                <div className="homepage-rotaract-ellipse-10" />
+                <div className="homepage-rotaract-ellipse-12" />
+              </div>
+              <div className="homepage-rotaract-overlap-group">
+                <div className="homepage-rotaract-ellipse-7" />
+                <div className="homepage-rotaract-ellipse-8" />
+              </div>
+            </div>
+            <div className="homepage-rotaract-ellipse-11" />
+            <div className="homepage-rotaract-ellipse-9" />
+          </div>
+        </div>
+        <div className="homepage-rotaract-ellipse-13" />
+        <div className="homepage-rotaract-div" />
+        <img className="homepage-rotaract-ornament" alt={"Ornament"} src={"ornament-14.jpg"} />
+        <img className="homepage-rotaract-ornament-76" alt={"Ornament"} src={"ornament-76.jpg"} />
+        <img className="homepage-rotaract-rectangle-6" alt={"Rectangle"} src={"rectangle-6.svg"} />
+        <img className="homepage-rotaract-rectangle-10" alt={"Rectangle"} src={"rectangle-10.svg"} />
+        <img className="homepage-rotaract-rectangle-11" alt={"Rectangle"} src={"rectangle-11.svg"} />
+        <img className="homepage-rotaract-rectangle-9" alt={"Rectangle"} src={"rectangle-9.svg"} />
+        <div className="homepage-rotaract-text-wrapper">WHO ARE WE?</div>
+        <p className="homepage-rotaract-rotaract-district-organisation-RID-happens-to-be-the-largest-youth-organisation-that-is-active-in-the-city-of-kolkata-it-s-suburbs-with-active-and-running-clubs-with-a-membership-count-close-to-formed-in-after-it-was-redistricted-from-erstwhile-district-it-covers-kolkata-and-other-revenue-districts-of-southern-west-bengal-and-andaman-and-nicobar-islands-in-india">
+          Rotaract District Organisation, RID 3291 happens to be the largest youth organisation that is active in the city of Kolkata
+          &amp; it&#39;s suburbs with 92 active and running clubs with a membership count close to 2500. Formed in 2008, after it was
+          redistricted from erstwhile district 3290. It covers Kolkata and 11 other revenue districts of Southern West Bengal and Andaman
+          and Nicobar Islands in India.
+        </p>
+        <div className="homepage-rotaract-hello-there">Hello, There!</div>
+        <div className="homepage-rotaract-text-wrapper-2">130</div>
+        <div className="homepage-rotaract-text-wrapper-3">Clubs</div>
+        <div className="homepage-rotaract-text-wrapper-4">3000</div>
+        <div className="homepage-rotaract-text-wrapper-5">Members</div>
+        <div className="homepage-rotaract-text-wrapper-6">3000</div>
+        <div className="homepage-rotaract-text-wrapper-7">Projects</div>
+        <div className="homepage-rotaract-text-wrapper-8">8</div>
+        <div className="homepage-rotaract-text-wrapper-9">Zones</div>
+        <img className="homepage-rotaract-group-2" alt={"Group"} src={"group-3.png"} />
+        <img className="homepage-rotaract-group-28" alt={"Group"} src={"group-28.png"} />
+        <img className="homepage-rotaract-group-3" alt={"Group"} src={"group-4.png"} />
+        <img className="homepage-rotaract-mask-group" alt={"Mask group"} src={"mask-group.png"} />
+        <div className="homepage-rotaract-rectangle-27" />
+        <div className="homepage-rotaract-rectangle-3" />
+        <div className="homepage-rotaract-group-23">
+          <img className="homepage-rotaract-ornament-16" alt={"Ornament"} src={"ornament-16.jpg"} />
+          <img className="homepage-rotaract-ornament-71" alt={"Ornament"} src={"ornament-71.jpg"} />
+        </div>
+        <div className="homepage-rotaract-text-wrapper-10">Rtn. Shekhar Mehta</div>
+        <p className="homepage-rotaract-p">RI President 2021 - 22</p>
+        <img className="homepage-rotaract-rectangle-4" alt={"Rectangle"} src={"rectangle-4.png"} />
+        <p className="homepage-rotaract-text-wrapper-11">LEADERS RY 2021 - 22</p>
+        <div className="homepage-rotaract-know-our-leaders">Know Our Leaders!</div>
+        <div className="homepage-rotaract-text-wrapper-12">AVENUES OF SERVICE</div>
+        <div className="homepage-rotaract-text-wrapper-13">What We Do?</div>
+        <p className="homepage-rotaract-we-channel-our-commitment-to-service-at-home-and-abroad-through-four-avenues-of-service-which-are-the-foundation-of-rotaract-club-activity">
+          We Channel Our Commitment To Service At Home And Abroad Through Four Avenues Of Service, Which Are The Foundation Of Rotaract
+          Club Activity.
+        </p>
+        <p className="homepage-rotaract-text-wrapper-14">
+          Shekhar Mehta, chair and founder of real estate development company, Skyline Group, was confirmed president-nominee of Rotary
+          International - a global network of people committed to improving lives and bringing positive, lasting change in communities
+          around the world.
+        </p>
+        <div className="homepage-rotaract-frame">
+          <img className="homepage-rotaract-arrow-right-outline" alt={"Arrow right outline"} src={"arrow-right-outline-24.svg"} />
+        </div>
+        <div className="homepage-rotaract-frame-8189">
+          <img className="homepage-rotaract-arrow-right-outline-24" alt={"Arrow right outline"} src={"arrow-right-outline-24-1.svg"} />
+        </div>
+        <div className="homepage-rotaract-rectangle-2" />
+        <div className="homepage-rotaract-rectangle-28" />
+        <div className="homepage-rotaract-text-wrapper-15">Community Service</div>
+        <div className="homepage-rotaract-text-wrapper-16">International Service</div>
+        <p className="homepage-rotaract-text-wrapper-17">
+          Community Service encourages every Rotaractor to find ways to improve the quality of life for people in their communities and to
+          serve the public interest.
+        </p>
+        <p className="homepage-rotaract-text-wrapper-18">
+          International Service exemplifies our global reach in promoting peace and understanding. We support this service avenue by
+          sponsoring or volunteering on international projects.
+        </p>
+        <div className="homepage-rotaract-rectangle-5" />
+        <div className="homepage-rotaract-rectangle-29" />
+        <div className="homepage-rotaract-text-wrapper-19">Professional Development</div>
+        <div className="homepage-rotaract-text-wrapper-20">Club Service</div>
+        <p className="homepage-rotaract-text-wrapper-21">
+          Community Service encourages every Rotaractor to find ways to improve the quality of life for people in their communities and to
+          serve the public interest.
+        </p>
+        <p className="homepage-rotaract-text-wrapper-22">
+          Club Service focuses on making clubs strong. A thriving club is anchored by strong relationships and an active membership
+          development plan.
+        </p>
+        <div className="homepage-rotaract-ellipse-14" />
+        <div className="homepage-rotaract-ellipse-15" />
+        <div className="homepage-rotaract-ellipse-16" />
+        <div className="homepage-rotaract-ellipse-17" />
+        <img className="homepage-rotaract-social-community" alt={"Social community"} src={"social-community-1.svg"} />
+        <img className="homepage-rotaract-global-team" alt={"Global team"} src={"global-team-1.svg"} />
+        <img className="homepage-rotaract-group-4" alt={"Group"} src={"group-1.svg"} />
+        <img className="homepage-rotaract-speaker" alt={"Speaker"} src={"speaker-1.svg"} />
+        <img className="homepage-rotaract-rectangle-7" alt={"Rectangle"} src={"rectangle-27.png"} />
+        <div className="homepage-rotaract-text-wrapper-23">Explore</div>
+        <div className="homepage-rotaract-text-wrapper-24">Contact Us</div>
+        <p className="homepage-rotaract-about-us-rotary-rotaract-DRR-theme-annual-plan-district-council-house-of-presidents-clubs-downloads-leaderboard">
+          About Us
+          <br />
+          Rotary &amp; Rotaract
+          <br />
+          DRR Theme
+          <br />
+          Annual Plan
+          <br />
+          District Council
+          <br />
+          House of Presidents
+          <br />
+          Clubs
+          <br />
+          Downloads
+          <br />
+          Leaderboard
+        </p>
+        <div className="homepage-rotaract-text-wrapper-25">+91-9073233810</div>
+        <div className="homepage-rotaract-text-wrapper-26">Address</div>
+        <div className="homepage-rotaract-text-wrapper-27">PP RTR. Rajarshi Adhikary</div>
+        <p className="homepage-rotaract-text-wrapper-28">50/B, HRIDAYPUR STATION ROAD, KAILASHNAGAR, HRIDAYPUR, BARASAT KOLKATA-700127</p>
+        <div className="homepage-rotaract-text-wrapper-29">Principal Aide to DRR</div>
+        <div className="homepage-rotaract-text-wrapper-30">Rotaract District Organisation</div>
+        <div className="homepage-rotaract-text-wrapper-31">Rotary International District 3291</div>
+        <img className="homepage-rotaract-image" alt={"Image"} src={"image-1.png"} />
+        <div className="homepage-rotaract-text-wrapper-32">Newsletter</div>
+        <div className="homepage-rotaract-rectangle-58" />
+        <div className="homepage-rotaract-text">Email Address</div>
+        <Button
+          color="Primary"
+          icon={false}
+          plusOutlineStyle={{
+            color: "#333333",
+            fontSize: "12px",
+            marginBottom: "-3.00px",
+            marginTop: "-5.00px",
+          }}
+          rounded="Default"
+          size="Medium"
+          state="Default"
+          style={{
+            backgroundColor: "#fbb305",
+            borderRadius: "99999997952px",
+            height: "40px",
+            left: "1705px",
+            overflow: "unset",
+            position: "absolute",
+            top: "3532px",
+            width: "98px",
+          }}
+          text="Subscribe"
+          type="Solid"
+        />
+        <img className="homepage-rotaract-icon-instagram" alt={"Icon instagram"} src={"logo-instagram-24.svg"} />
+        <img className="homepage-rotaract-icon-facebook" alt={"Icon facebook"} src={"logo-facebook-24.svg"} />
+        <img className="homepage-rotaract-icon-linkedin" alt={"Icon linkedin"} src={"linkedin-fill.svg"} />
+        <img className="homepage-rotaract-youtube-fill" alt={"Youtube fill"} src={"youtube-fill.svg"} />
+        <img className="homepage-rotaract-mask-group-2" alt={"Mask group"} src={"mask-group-1.png"} />
+        <div className="homepage-rotaract-overlap-wrapper">
+          <div className="homepage-rotaract-overlap-group4">
+            <div className="homepage-rotaract-frame-8184">
+              <img className="homepage-rotaract-article-outline" alt={"Article outline"} src={"article-outline-24.svg"} />
+              <div className="homepage-rotaract-text-wrapper-33">Menu</div>
+            </div>
+            <div className="homepage-rotaract-text-wrapper-34">Clubs</div>
+            <div className="homepage-rotaract-text-wrapper-35">Leaderboard</div>
+            <div className="homepage-rotaract-text-wrapper-36">Council</div>
+            <div className="homepage-rotaract-text-wrapper-37">Annual Plan</div>
+            <div className="homepage-rotaract-text-wrapper-38">About Us</div>
+            <img className="homepage-rotaract-image-1" alt={"Image"} src={"image-1.png"} />
+          </div>
+        </div>
+        <img className="homepage-rotaract-group-5" alt={"Group"} src={"group-1.png"} />
+        <h1 className="homepage-rotaract-h-1">Rotaract District Organisation</h1>
+        <div className="homepage-rotaract-text-wrapper-39">Rotary International District 3291</div>
+        <p className="homepage-rotaract-kolkata-south-bengal-andamans">Kolkata, South Bengal &amp; Andamans</p>
+        <img className="homepage-rotaract-li-before" alt={"Li before"} src={"li-before.png"} />
+        <img className="homepage-rotaract-li-before-2" alt={"Li before"} src={"li-before.png"} />
+        <img className="homepage-rotaract-li-before-3" alt={"Li before"} src={"li-before.png"} />
+        <img
+          className="homepage-rotaract-FINAL-LOGO-ROTARY-WHITE-TRANSPARENT"
+          alt={"Final LOGO ROTARY WHITE TRANSPARENT"}
+          src={"final-logo-rotary-white-transparent-1.png"}
+        />
+        <img
+          className="homepage-rotaract-FINAL-LOGO-ROTARY-WHITE-TRANSPARENT-2"
+          alt={"Final LOGO ROTARY WHITE TRANSPARENT"}
+          src={"final-logo-rotary-white-transparent-1.png"}
+        />
+        <img
+          className="homepage-rotaract-FINAL-LOGO-ROTARY-WHITE-TRANSPARENT-3"
+          alt={"Final LOGO ROTARY WHITE TRANSPARENT"}
+          src={"final-logo-rotary-white-transparent-1.png"}
+        />
+        <img
+          className="homepage-rotaract-FINAL-LOGO-ROTARY-WHITE-TRANSPARENT-4"
+          alt={"Final LOGO ROTARY WHITE TRANSPARENT"}
+          src={"final-logo-rotary-white-transparent-1.png"}
+        />
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
---
+++ 3291/src/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+function Button() {
+  return (
+    <div className="Remotewa">
+        <button>custom button</button>
+    </div>   
+    );
+}
+
+export {Button};
